d27ec1291ce22e2001a536b8a5af963df5c754ce
Kgati <valendahthaba@gmail.com>
2026-03-12 17:31:12 +0200

    Files added

---
+++ Hello.java
@@ -0,0 +1,10 @@
+public class Hello {
+	String[] names = {"K Thaba", "S Ndlovu", "G Nhlabathi"};
+
+	public static void  main(String[]args) {
+		Hello name = new Hello();
+			for(String n : name.names) {
+		System.out.println("Hello Git" + n + ", Welecome to coding"); 
+    }
+  }
+}
