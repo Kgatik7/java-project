8551b2366343760e9970f2344180f643cbd708b2
Kgati <valendahthaba@gmail.com>
2026-03-12 17:39:07 +0200

    Added new feature

--- Hello.java
+++ Hello.java
@@ -4,7 +4,8 @@ public class Hello {
 	public static void  main(String[]args) {
 		Hello name = new Hello();
 			for(String n : name.names) {
-		System.out.println("Hello Git" + n + ", Welecome to coding"); 
+		System.out.println("Hello" + n + ", Welecome to coding");
+		System.out.println("New Feature Added"); 
     }
   }
 }
